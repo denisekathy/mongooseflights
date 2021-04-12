fbdc9858d7e11ef065ba158b80b9522853ee83b3
Denise Onukwue <donukwue1@gmail.com>
2021-04-12 17:41:36 -0400

    update routes but add ticket function is broken

--- controllers/tickets.js
+++ controllers/tickets.js
@@ -4,7 +4,7 @@ const Flight = require("../models/flight");
 module.exports = {
     new: newTicket,
     create,
-    addToFlight,
+   
   };
 
   function newTicket(req, res) {
@@ -20,22 +20,21 @@ module.exports = {
 
 
   function create(req, res) {
-    const ticket = new Ticket(req.body);
-    ticket.save(function (err) {
-      if (err) return res.redirect("/tickets/new");
-      res.redirect("/tickets/new"); 
-    //   res.redirect(`/flights/${flight._id}`);   
-    });
+    Flight.findById(req.params.id, function(err, flight) {
+        const ticket = new Ticket(req.body);
+        ticket.flight = flight._id;
+        ticket.save(function(err) {
+            res.redirect(`/flights/${flight._id}/tickets/new`)
+    // req.body.flight = req.params.id
+    // Ticket.create(req.body, function(err, tickets) {
+    //   res.redirect(`/flights/${req.params.id}`)
 
-}
-  function addToFlight(req, res) {
-    Flight.findById(req.params.id, function (err, flight) {
-        Ticket.find({flight: flight._id}, function (err, ticket){
-            flight.save(function (err) {
-              res.redirect(`/flights/${flight._id}`);
-              
-        })
-       
-      });
-    });
-}
+    // const ticket = new Ticket(req.body);
+    // ticket.save(function (err) {
+    //   if (err) return res.redirect("/tickets/new");
+    // //   res.redirect("/tickets/new");  
+    //   res.redirect(`/flights/${flight._id}`);   
+    })
+
+} )
+  }
